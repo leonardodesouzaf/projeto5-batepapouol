b34b35005efede7782fe9f0535ed33dc118360ea
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-06-26 00:33:41 -0300

    enter-in-room-done

--- scripts.js
+++ scripts.js
@@ -1,14 +1,75 @@
-let participantname = prompt('Defina um nome de usuário: ');
-let participant = {
-    name: participantname
+let participant;
+let participantname;
+
+function requestName(){
+    participantname = prompt('Defina um nome de usuário: ');
+    participant = {
+        name: participantname
+    }
+    const postpromise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', participant);
+    postpromise.then(inputName);
+    postpromise.catch(requestName);
 }
-const postpromise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', participant);
-postpromise.then(displaying);
 
+requestName();
 
-function displaying(resposta){
-    console.log(resposta);
+function inputName(resposta){
+    if(resposta.status === 200){
+        getMessages();
+        setInterval(getMessages, 3000);
+        setInterval(stayConnected, 5000);
+    }
 }
 
+function stayConnected(){
+    const postpromise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', participant);
+}
+
+
+function getMessages(){
+    const getpromise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    getpromise.then(displayMessages);
+}
+
+function displayMessages(resposta){
+    let messagebox = document.querySelector('.messages');
+    messagebox.innerHTML='';
+    let data = resposta.data;
+    for(let i=0; i<data.length; i++){
+        if(data[i].type === "status"){
+            messagebox.innerHTML+= `
+                <div class="status-message">
+                <div class="hour">(${data[i].time}) </div>
+                <div class="name"> ${data[i].from} </div>
+                <div class="action-inout"> ${data[i].text}</div>
+            </div>
+            `;
+        }
+        if(data[i].type === "private_message"){
+            messagebox.innerHTML+= `
+                <div class="private-message">
+                <div class="hour">(${data[i].time}) </div>
+                <div class="name"> ${data[i].from} </div>
+                <div class="message-to"> reservadamente para</div>
+                <div class="message-to-person"> ${data[i].to}:</div>
+                <div class="message-sent">${data[i].text}</div>
+            </div>
+            `;
+        }
+        if(data[i].type === "message"){
+            messagebox.innerHTML+= `
+                <div class="message">
+                <div class="hour">(${data[i].time}) </div>
+                <div class="name"> ${data[i].from} </div>
+                <div class="message-to"> para</div>
+                <div class="message-to-person"> ${data[i].to}:</div>
+                <div class="message-sent">${data[i].text}</div>
+            </div>
+            `;
+        }
+    }
+    messagebox.innerHTML+=`<div class="new-messages"></div>`;
+    document.querySelector('.new-messages').scrollIntoView({block: "end", inline: "nearest", behavior: "smooth"});
+}
 
 
